f98a741d0afbea6924abf10a742e5cdc77b0af9e
mayankjhaCA <36353432+mayankjhaCA@users.noreply.github.com>
2021-11-12 13:54:06 -0800

    Update WebsiteMeta.js

--- src/components/common/meta/WebsiteMeta.js
+++ src/components/common/meta/WebsiteMeta.js
@@ -64,13 +64,6 @@ const WebsiteMeta = ({ data, settings, canonical, title, description, image, typ
                 {settings.twitter && <meta name="twitter:site" content={`https://twitter.com/${settings.twitter.replace(/^@/, ``)}/`} />}
                 {settings.twitter && <meta name="twitter:creator" content={settings.twitter} />}
                 <script type="application/ld+json">{JSON.stringify(jsonLd, undefined, 4)}</script>
-                <!-- Google Tag Manager -->
-                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-K4GWX9V');</script>
-                <!-- End Google Tag Manager -->
             </Helmet>
             <ImageMeta image={shareImage} />
         </>
