ae46eff3a046319084a6f0b3450ff2b4d8862080
mayankjhaCA <36353432+mayankjhaCA@users.noreply.github.com>
2021-11-12 13:49:42 -0800

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -27,13 +27,6 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
         <>
             <Helmet>
                 <html lang={site.lang} />
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K4GWX9V');</script>
-<!-- End Google Tag Manager -->
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
                 <body className={bodyClass} />
             </Helmet>
